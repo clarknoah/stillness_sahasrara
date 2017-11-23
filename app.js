453055831a2b87b040227279a6818c65efe92538
clarknoah <noahbc08@gmail.com>
2017-11-23 12:47:49 -0500

    Added routing stuff

--- app.js
+++ app.js
@@ -41,6 +41,29 @@ app.get('/getConceptList', function(req, res){
     });
 });
 
+app.get('/getConceptQualiaList', function(req, res){
+  var requestVars = {
+    concept_label: 'Concept',
+    qualia_field_name: 'label_name'
+  };
+  var session = driver.session();
+  session
+    .run( 'MATCH (c:'+requestVars.concept_label+')-[]->(q:Qualia)' +
+          'WHERE (c).label_name="'+requestVars.concept_label+'" ' +
+          'RETURN collect(properties(q)) as qualiaProperties')
+    .then(function(result){
+      session.close();
+        var concept = {};
+      concept.properties = result.records[0]._fields[0]
+
+  console.log(concept.properties);
+  //console.log(JSON.stringify(result.records[0]._fields[0]));
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+});
+
 app.get('/getConceptForm', function(req,res){
 
 })
---
+++ routes/getConceptQualias.js
@@ -0,0 +1,24 @@
+var neo4j = require('neo4j-driver').v1;
+
+exports.getConcepList = function(req, res){
+  var requestVars = {
+    concept_label: 'Concept',
+    qualia_field_name: 'label_name'
+  };
+  var session = driver.session();
+  session
+    .run( 'MATCH (c:'+requestVars.concept_label+')-[]->(q:Qualia)' +
+          'WHERE (c).label_name="'+requestVars.concept_label+'" ' +
+          'RETURN collect(properties(q)) as qualiaProperties')
+    .then(function(result){
+      session.close();
+        var concept = {};
+      concept.properties = result.records[0]._fields[0]
+
+  console.log(concept.properties);
+  //console.log(JSON.stringify(result.records[0]._fields[0]));
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+}
