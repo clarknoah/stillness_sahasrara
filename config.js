389a35ffdfac60181a091d0f2a99fb5d5b57dbbc
clarknoah <noahbc08@gmail.com>
2018-03-08 11:15:58 -0500

    fixed password issue

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 var neoHost;
+var password = 'neo4j';
 var env = process.env.NODE_ENV;
 console.log(env);
 if(env==='dev'){
   neoHost = '0.0.0.0';
+  password = 'stillness';
 }else if(env === 'test' || env === 'prod'){
   neoHost = 'neo4j';
 }else{
@@ -13,7 +15,7 @@ const serverConfig = {
   //neo4j_local:"bolt://0.0.0.0",
   neo4j_local:"bolt://"+neoHost+':7687',
   user_local:"neo4j",
-  password_local:"stillness"
+  password_local:password
 };
 
 module.exports = serverConfig;
